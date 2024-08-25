0edea6109f84cfe8882459802af562ce85ad2fc8
yo <johan@nosd.in>
2024-08-25 21:47:37 +0200

    HashCode now return 0 for empty string, to allow specifying empty cache name when specifying schema in QuerySQLFields

--- binary/v1/hash-code.go
+++ binary/v1/hash-code.go
@@ -8,7 +8,8 @@ func HashCode(s string) int32 {
 // HashCodeForSlice calculates Java hash code for byte array
 func HashCodeForSlice(b []byte) int32 {
 	if len(b) == 0 {
-		return 1
+		// HashCode of empty string return 0 so it can be used in QuerySQLFields with no TableName but a Schema
+		return 0
 	}
 
 	h := uint32(0)
